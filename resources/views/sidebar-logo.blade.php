3dc146dd3b0bfcf9bcb783279aa3ebd56ca0144b
FahrulID <fahrulofficial@gmail.com>
2024-06-06 17:49:34 +0700

    initial commit

---
+++ resources/views/sidebar-logo.blade.php
@@ -0,0 +1,7 @@
+<div class="flex">
+    <img src="{{ asset('images/logo.png') }}" alt="" srcset="" width="24">
+    <div class="fi-logo flex text-xl font-bold tracking-tight text-gray-950 dark:text-white leading-[24px]"
+        style="margin-left: 0.4rem">
+        AHLI WARIS
+    </div>
+</div>
