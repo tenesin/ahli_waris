9d5fd101a95bef953fb516de9c0a621f64cdfcf3
ishaqyudha01@gmail.com <ishaqyudha01@gmail.com>
2024-11-06 13:53:30 +0700

    add: pusat edukasi page

---
+++ app/Filament/Pages/PusatEdukasi.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Page;
+
+class PusatEdukasi extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.pages.pusat-edukasi';
+}
